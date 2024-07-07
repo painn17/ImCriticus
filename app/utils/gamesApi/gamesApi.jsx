af7540537271ddf91d94b59bcc4cf40788153e6d
painn17 <sharikovillya14@gmail.com>
2024-07-07 13:38:02 +0300

    _

--- app/utils/gamesApi/gamesApi.jsx
+++ app/utils/gamesApi/gamesApi.jsx
@@ -1,6 +1,5 @@
 import FetchData from "../fetch";
 export async function getGames() {
-  const key = "0bc76dcac2314ee99b453f779f7a5bb6";
   let data = await FetchData(
     `https://api.rawg.io/api/games?key=${key}&?page_size=200`
   );
